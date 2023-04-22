32fc3a8c89853d148eb63d04050e8b8472278daf
Dorotea Dragojevic <dorotea.dragojevic@fer.hr>
2023-04-22 22:32:32 +0200

    initial commit

---
+++ src/main/java/fer/hr/zavrsni/PracenjeIPlaniranjePutovanjaApplication.java
@@ -0,0 +1,13 @@
+package fer.hr.zavrsni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracenjeIPlaniranjePutovanjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracenjeIPlaniranjePutovanjaApplication.class, args);
+	}
+
+}
